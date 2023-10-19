0861216c60e030eac6f0384e430be32f21706285
AveJonk <zoyzaidan30@gmail.com>
2023-10-19 09:07:25 +0700

    tugas 3

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 7895
+const data_users = [
+    { id: 1, name: "Ijong", komentar: "Bantuin dong" },
+    { id: 2, name: "Yuuichi", komentar: "gantiin gw" },
+    { id: 3, name: "Misterius", komentar: "Jakarta keren ya" },
+    { id: 4, name: "Lala", komentar: "Pusing banget" },
+];
 
+app.get("/users", (req, res) => {
+    //mendapatkan data dari database
+    const data = data_users;
+
+    //memberikan respon json data
+    let result = {
+        status: 200,
+        data: data,
+    };
+
+    res.json(result);
+});
 
 app.get("/", (req, res) => {
     res.json({
